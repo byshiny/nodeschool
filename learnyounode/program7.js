c29093e0cb002dc7597528d43a2d8bc1d3a08ac3
bs5ht <bs5ht@virginia.edu>
2017-12-15 22:08:16 -0500

    exercise 7 completed

---
+++ learnyounode/program7.js
@@ -0,0 +1,10 @@
+
+function callback(response){
+  response.setEncoding('utf8');
+  response.on("data", function(data)
+  {
+    console.log(data);
+  });
+}
+var http = require('http');
+http.get(process.argv[2], callback);
