341166438b6fb0ebb816aec24cfc96173f5d3c9f
byshiny <byshiny@github.com>
2018-01-13 23:48:41 -0500

    program7 done

--- sa/program7.js
+++ sa/program7.js
@@ -1,6 +1,12 @@
 var http = require('http');
 var fs = require('fs');
+var through2 = require('through2');
 var server = http.createServer(function (req, res) {
-    fs.createReadStream('file.txt').pipe(res);
+    req.pipe(through2(function (line, _, next) {
+
+        this.push(line.toString().toUpperCase());
+
+      next();
+    })).pipe(res);
 });
 server.listen(process.argv[2]);
