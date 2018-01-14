ee4cc7c23035eeafe749f376b0bd2cf370fd9160
byshiny <bs5ht@virginia.edu>
2018-01-14 03:27:31 -0500

    fixed this damn ws issue

--- sa/program9.js
+++ sa/program9.js
@@ -1,5 +1,7 @@
 var ws = require('websocket-stream');
-var stream = ws('http://localhost:8099');
+var stream = ws('ws://localhost:8099');
 //stream.write("hello\n");
 //stream.write("There's a place I know just east of here.");
-stream.end('Hello\n');
+stream.end('hello\n');
+
+//DO NOT REPLACE ws with http!!!!
