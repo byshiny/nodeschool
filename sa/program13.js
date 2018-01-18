24249685608c37a403b6ab628ead004fef015024
byshiny <bs5ht@virginia.edu>
2018-01-18 01:35:29 -0500

    omg too much for one exercise q.q

--- sa/program13.js
+++ sa/program13.js
@@ -1,113 +1,84 @@
 var combine = require('stream-combiner');
 var t2 = require('through2');
 var split = require('split');
-
+var zlib = require('zlib');
 module.exports = function() {
 
-    //define a new stream here
-    var Readable = require('stream').Readable;
-    var stream = new Readable({
-      objectMode: true
-    });
+  //define a new stream here
+  var Readable = require('stream').Readable;
+  var stream = new Readable({
+    objectMode: true
+  });
 
-var mappy = {};
-    var splitInstance = split({trailing:false});
-    splitInstance.on('data', function(line) {
+  var mappy = {};
+  var count = 0;
+  var t2inst = t2.obj(function(obj, enc, callback) {
+    //console.log(ticker + obj);
+    obj = JSON.parse(obj);
 
-      var entry = line;
-      if(entry.type == "genre"){
-        mappy.name = entry.name;
-        mappy.books =[];
+    if (obj.type == "genre") {
+      if (count > 1) {
+        //mappy.books.sort();
+        t2inst.push(JSON.stringify(mappy) + '\n');
       }
-      else{
-        mappy.books.push(entry.name);
-      }
-
-    });
-    splitInstance.on('end', function(line) {
-    });
-
-
-
-    /*
-    t2.obj(function(chunk, enc, callback) {
-        console.log('copout');
-        this.push(chunk);
-        callback();
-    })*/
-    return combine(
-      splitInstance, process.stdout
-
-      // read newline-separated json,
-      // group books into genres,
-      // then gzip the output
-    );
+      mappy.name = obj.name;
+      mappy.books = [];
+    } else {
+      mappy.books.push(obj.name);
+    }
+    count++;
+    //this.push(chunk);
+    callback();
+  }, function(cb) {
+    //smappy.books.sort();
+    t2inst.push(JSON.stringify(mappy) + '\n');
+    cb();
+  });
+  return combine(
+    split(null, null, {
+      trailing: false
+    }), t2inst, zlib.createGzip()
+  );
 
 }
+/* NOTES
+CHECK NEW LINE CHARACTER
+CALL CALLBACK AFTER DONE
+SPLIT IS CALLED TO REMOVE NEW LINES
+OMG THIS TOOK MUCH LONGER THAN EXPECTED Q.Q
+TAKE CARE IN PASSING OBJS
+// Here's the reference solution:
 
+  var combine = require('stream-combiner');
+  var through = require('through2');
+  var split = require('split');
+  var zlib = require('zlib');
 
+  module.exports = function () {
+      var grouper = through(write, end);
+      var current;
 
-    /*
-    var duplexer2 = require('duplexer2');
-    var stream = require('stream');
-    var through = require('through2');
-    module.exports = function (counter) {
-        // spawn the process and return a single stream
-        // joining together the stdin and stdout here
-      var readable = new stream.Readable();
+      function write (line, _, next) {
+          if (line.length === 0) return next();
+          var row = JSON.parse(line);
 
-      var countMap = {};
-      var writable = new stream.Writable({objectMode: true});
-       writable._write = function _write(input, encoding, done) {
-         if(countMap[input.country] == null){
-           countMap[input.country] = 1;
-         }
-         else{
-           countMap[input.country]++;
-         }
-         if (readable.push(JSON.stringify(input))) {
-           return done();
-         } else {
-           readable.once("drain", done);
-         }
-       };
-
-
-       writable.on('finish', function() {
-        // console.log(countMap);
-         counter.setCounts(countMap);
-
-       });
-
-       readable._read = function _read(n) {
-         // no-op
-       };
-
-
-        var dup = duplexer2({writableObjectMode: true}, writable, counter);
-
-
-    return dup;
-    };
-    /*
-    // Here's the reference solution:
-
-      var duplexer = require('duplexer2');
-      var through = require('through2').obj;
-
-      module.exports = function (counter) {
-          var counts = {};
-          var input = through(write, end);
-          return duplexer({objectMode: true}, input, counter);
-
-          function write (row, _, next) {
-              counts[row.country] = (counts[row.country] || 0) + 1;
-              next();
+          if (row.type === 'genre') {
+              if (current) {
+                  this.push(JSON.stringify(current) + '\n');
+              }
+              current = { name: row.name, books: [] };
           }
-          function end (done) {
-              counter.setCounts(counts);
-              done();
+          else if (row.type === 'book') {
+              current.books.push(row.name);
           }
-      };
-    */
-    //derp swapped the inputs and outputs. Think carefully next time!
+          next();
+      }
+      function end (next) {
+          if (current) {
+              this.push(JSON.stringify(current) + '\n');
+          }
+          next();
+      }
+
+      return combine(split(), grouper, zlib.createGzip());
+  };
