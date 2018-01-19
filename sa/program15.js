ef4a1baed338f603c85f26a1a856aed2bc2346ed
byshiny <bs5ht@virginia.edu>
2018-01-18 22:02:46 -0500

    last challenge

---
+++ sa/program15.js
@@ -0,0 +1,8 @@
+var crypto = require('crypto');
+var tar = require('tar');
+    var parser = tar.Parse();
+    parser.on('entry', function (e) {
+        console.dir(e);
+    });
+    var fs = require('fs');
+    fs.createReadStream('file.tar').pipe(parser);
