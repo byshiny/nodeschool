e42fe3c303846a6e1c589f18f551205e87a1d00a
byshiny <bs5ht@virginia.edu>
2018-01-19 23:56:34 -0500

    p15 almost done

--- sa/program15.js
+++ sa/program15.js
@@ -1,8 +1,25 @@
 var crypto = require('crypto');
 var tar = require('tar');
+var zlib = require('zlib');
     var parser = tar.Parse();
     parser.on('entry', function (e) {
-        console.dir(e);
+      console.log('herro');
+        //console.dir(e);
     });
-    var fs = require('fs');
-    fs.createReadStream('file.tar').pipe(parser);
+    //fs.createReadStream('file.tar').
+
+/*
+    var t2 = t2.obj(function(obj, enc, callback) {
+      //console.log(ticker + obj);
+      obj = JSON.parse(obj);
+      var retStr = entry.type + entry.path
+      console.log(obj);
+      callback();
+    }, function(cb) {
+      //smappy.books.sort();
+      t2inst.push(JSON.stringify(mappy) + '\n');
+      cb();
+    });*/
+
+    var cryptoStream = crypto.createDecipher(process.argv[2], process.argv[3]);
+    process.stdin.pipe(cryptoStream).pipe(zlib.createGunzip()).pipe(parser);
